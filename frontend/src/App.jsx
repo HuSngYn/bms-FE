5c688e708ca38b4ac02b98d71c2ae20aeb3eaf28
seunghwan <74578005+yush0822@users.noreply.github.com>
2025-12-08 14:34:17 +0900

    승환 수정 버전

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,12 @@ import Layout from "./components/layout/Layout.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import BookListPage from "./pages/BookListPage.jsx";
 import BookCreatePage from "./pages/BookCreatePage.jsx";
+import LogInPage from "./pages/LogInPage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import BookDetailPage from "./pages/BookDetailPage.jsx";
+import BookEditPage from "./pages/BookEditPage.jsx";
+import ApiTestPage from "./pages/ApiTestPage.jsx";
+import MyBookListPage from "./pages/MyBookListPage.jsx";
 
 function App() {
   return (
@@ -14,6 +20,13 @@ function App() {
         <Route path="books" element={<BookListPage />} />
         <Route path="books/new" element={<BookCreatePage />} />
         {/* 추후: <Route path="books/:id" element={<BookDetailPage />} /> 등 추가 */}
+        <Route path="login" element={<LogInPage />} />
+        <Route path="signup" element={<SignUpPage />} />
+        <Route path="books/:id" element={<BookDetailPage />} />
+        <Route path="books/:id/edit" element={<BookEditPage />} />
+        <Route path="api_test" element={<ApiTestPage />} />
+        <Route path="my/books" element={<MyBookListPage />} />
+
         <Route path="*" element={<div>페이지를 찾을 수 없습니다.</div>} />
       </Route>
     </Routes>
